d22c7e73f1be6f3eaab57da7ce9be94cbba8f276
edward <bepyan@naver.com>
2022-05-28 01:01:52 +0900

    test. env

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,7 +3,7 @@ import { withHandler } from '@middlewares';
 export default withHandler({
   public: {
     GET: (req, res) => {
-      res.status(200).json({ ok: true, message: 'HELLO WORLD' });
+      res.status(200).json({ ok: true, message: process.env.TEST_URL || 'HELLO WORLD' });
     },
   },
 });
